6a6c627b81e8c1ebfbd2747c224b74a03f475647
gerardomdn95 <gerardo.mdn95@gmail.com>
2018-01-25 16:30:10 -0600

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,21 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { SocialLoginModule, AuthServiceConfig } from 'angular4-social-login';
+import { GoogleLoginProvider, FacebookLoginProvider } from 'angular4-social-login';
 
 
 import { AppComponent } from './app.component';
+import { FormComponent } from './form/form.component';
+import { NavComponent } from './nav/nav.component';
+import { LoginComponent } from './login/login.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FormComponent,
+    NavComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule
