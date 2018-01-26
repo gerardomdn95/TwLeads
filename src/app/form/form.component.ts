5d2be8d65eeedaeed90b11326952b27a524a6682
gerardomdn95 <gerardo.mdn95@gmail.com>
2018-01-26 09:07:20 -0600

    login working

--- src/app/form/form.component.ts
+++ src/app/form/form.component.ts
@@ -7,8 +7,6 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FormComponent implements OnInit {
 
-  constructor() { }
-
   ngOnInit() {
   }
 
--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -30,6 +30,7 @@ export class LoginComponent implements OnInit {
   ngOnInit() {
     this.authService.authState.subscribe((user) => {
       this.user = user;
+      console.log(this.user);
     });
   }
 
